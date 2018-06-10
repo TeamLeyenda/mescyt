5ee42e1d002c7556c098284c4ae2a3f0761b221f
Cristian <crisflowbg11@gmail.com>
2018-06-10 18:14:55 -0400

    builder

--- backend/models/views/default/index.php
+++ backend/models/views/default/index.php
@@ -1,7 +1,31 @@
 <?php
 use meysampg\formbuilder\FormBuilder;
 ?>
+
 <?= FormBuilder::widget([
-    'language' => 'en-MX',
+'data' => [
+    [
+        "type" => "header",
+        "subtype" => "h1",
+        "label" => "Header",
+        "class" => "header",
+    ],
+    [
+        "type" => "button",
+        "label" => "Button",
+        "subtype" => "button",
+        "class" => "button-input btn btn-warning",
+        "name" => "button-1475845417456",
+        "style" => "warning",
+    ],
+],
+'language' => 'en',
+'elementType' => 'div',
+'dataType' => 'xml',
+'accessVariableName' => 'formBuilderJsVariable' 
+
 ]); ?>
+
+
+
 
