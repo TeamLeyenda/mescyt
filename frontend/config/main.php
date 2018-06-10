505da2cebd465e9bd035b59f2b3f68e5cae20985
Cristian <crisflowbg11@gmail.com>
2018-06-10 17:45:14 -0400

    bilder

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'build' => [
+            'class' => 'backend\models\FormBuilder',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
--- frontend/views/site/say.php
+++ frontend/views/site/say.php
@@ -1,4 +1,9 @@
 <?php
 use yii\helpers\Html;
+use meysampg\formbuilder\FormBuilder;
 ?>
 <?= Html::encode($message) ?>
+<?= FormBuilder::widget([
+    'dataType' => 'json',
+    'language' => 'en',
+]); ?>
