5bce94926260e32520bd6bce858f0d923e200794
Cesar Darinel Ortiz <cesardarinelortiz@gmail.com>
2018-06-19 00:24:26 -0400

    creando los htaccess para la url

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,6 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
+    'homeUrl' => '/administrator',
     'modules' => [
         /*
         'admin' => [
@@ -75,6 +76,7 @@ return [
         
         'request' => [
             'csrfParam' => '_csrf-backend',
+            'baseUrl' => '/administrator'
         ],
         /*
         'assignment' => [
