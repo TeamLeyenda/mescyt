f0540e322ab780095728fa1daed200cf8dff13fa
Cesar Darinel Ortiz <cesardarinelortiz@gmail.com>
2018-06-20 00:59:24 -0400

    template del frontend, adaptado a los celulares

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,50 +8,273 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'My Yii Application';
 $this->title = 'Inicio - MESCyT';
 ?>
-<div class="site-index">
+<!--Section: Jumbotron-->
+<section class="card wow fadeIn" style="background-image: url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg);">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <!-- Content -->
+    <div class="card-body text-white text-center py-5 px-5 my-5">
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <h1 class="mb-4">
+            <strong>Learn Bootstrap 4 with MDB</strong>
+        </h1>
+        <p>
+            <strong>Best & free guide of responsive web design</strong>
+        </p>
+        <p class="mb-4">
+            <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video and written
+                versions available. Create your own, stunning website.</strong>
+        </p>
+        <a target="_blank" href="https://mdbootstrap.com/bootstrap-tutorial/" class="btn btn-outline-white btn-lg">Start free tutorial
+            <i class="fa fa-graduation-cap ml-2"></i>
+        </a>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
+    <!-- Content -->
+</section>
+<!--Section: Jumbotron-->
 
-    <div class="body-content">
+<hr class="my-5">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+<!--Section: Cards-->
+<section class="text-center">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+    <!--Grid row-->
+    <div class="row mb-4 wow fadeIn">
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <div class="embed-responsive embed-responsive-16by9 rounded-top">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cXTThxoywNQ" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">MDB Quick Start</h4>
+                    <!--Text-->
+                    <p class="card-text">Get started with MDBootstrap, the world's most popular Material Design framework for building
+                        responsive, mobile-first sites.</p>
+                    <p class="card-text">
+                        <strong>5 minutes, a few clicks and... done. You will be surprised at how easy it is.</strong>
+                    </p>
+                </div>
+
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <!--/.Card-->
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
         </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <img src="https://mdbootstrap.com/wp-content/uploads/2017/11/brandflow-tutorial-fb.jpg" class="card-img-top" alt="">
+                    <a href="https://mdbootstrap.com/automated-app-start/" target="_blank">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Bootstrap Automation</h4>
+                    <!--Text-->
+                    <p class="card-text">Learn how to create a smart website which learns your user and reacts properly to his behavior.</p>
+                    <a href="https://mdbootstrap.com/automated-app-start/" target="_blank" class="btn btn-primary btn-md">Start tutorial
+                        <i class="fa fa-play ml-2"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <img src="https://mdbootstrap.com/wp-content/uploads/2018/01/push-fb.jpg" class="card-img-top" alt="">
+                    <a href="https://mdbootstrap.com/web-push-start/" target="_blank">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Push notifications</h4>
+                    <!--Text-->
+                    <p class="card-text">Push messaging provides a simple and effective way to re-engage with your users and in this
+                        tutorial you'll learn how to add push notifications to your web app</p>
+                    <a href="https://mdbootstrap.com/web-push-start/" target="_blank" class="btn btn-primary btn-md">Start tutorial
+                        <i class="fa fa-play ml-2"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
 
     </div>
-</div>
+    <!--Grid row-->
+
+    <!--Grid row-->
+    <div class="row mb-4 wow fadeIn">
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Marketing/mdb-press-pack/mdb-angular.jpg" class="card-img-top" alt="">
+                    <a href="https://mdbootstrap.com/angular/" target="_blank">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">MDB with Angular</h4>
+                    <!--Text-->
+                    <p class="card-text">Built with Angular 5, Bootstrap 4 and TypeScript. CLI version available. </p>
+                    <a href="https://mdbootstrap.com/angular/" target="_blank" class="btn btn-primary btn-md">Free download
+                        <i class="fa fa-download ml-2"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Marketing/mdb-press-pack/mdb-react.jpg" class="card-img-top" alt="">
+                    <a href="https://mdbootstrap.com/react/" target="_blank">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">MDB with React</h4>
+                    <!--Text-->
+                    <p class="card-text">Based on the latest Bootstrap 4 and React 16. </p>
+                    <a href="https://mdbootstrap.com/react/" target="_blank" class="btn btn-primary btn-md">Free download
+                        <i class="fa fa-download ml-2"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4">
+
+            <!--Card-->
+            <div class="card">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Marketing/mdb-press-pack/mdb-vue.jpg" class="card-img-top" alt="">
+                    <a href="https://mdbootstrap.com/vue/" target="_blank">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">MDB with Vue</h4>
+                    <!--Text-->
+                    <p class="card-text">Based on the latest Bootstrap 4 and Vue 2.5.7. </p>
+                    <a href="https://mdbootstrap.com/vue/" target="_blank" class="btn btn-primary btn-md">Free download
+                        <i class="fa fa-download ml-2"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
+
+    </div>
+    <!--Grid row-->
+
+    <!--Pagination-->
+    <nav class="d-flex justify-content-center wow fadeIn">
+        <ul class="pagination pg-blue">
+
+            <!--Arrow left-->
+            <li class="page-item disabled">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+
+            <li class="page-item active">
+                <a class="page-link" href="#">1
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">4</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">5</a>
+            </li>
+
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!--Pagination-->
+
+</section>
