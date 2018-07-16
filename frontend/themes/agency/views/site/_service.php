aea23c59998555779efeea5f9da622e621707d05
jmlemoine <jeanmelvinlp27@gmail.com>
2018-07-16 19:22:57 -0400

    Actualizando el frontend

--- frontend/themes/agency/views/site/_service.php
+++ frontend/themes/agency/views/site/_service.php
@@ -2,46 +2,27 @@
     <section id="services" style="background-color: #235288; border-color: #235288">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading" style="color: white">Servicios</h2>
-                    <h3 class="section-subheading text-muted" style="color: white">Nuestros servicios al usuario por ventanilla están disponibles en horario de 8:00 AM a 4:00 PM de lunes a viernes.</h3>
+                <div class="col-lg-12 text-left">
+                    <h2 class="section-heading" style="color: white">¡BIENVENIDOS A CEICYT RD-2018!</h2>
+                    <h3 class="section-subheading text-muted" style="color: white">El Congreso Estudiantil de Investigación Científica y Tecnológica (CEICYT) es un espacio para dar a conocer las iniciativas de investigación de estu- diantes universitarios de las áreas vinculadas a las ciencias básicas y aplicadas. Se llevará a cabo los días 9 y 10 de mayo de 2018 en el Campus Sa- nto Tomás de Aquino de la Pontificia Universidad Católica Madre y Maestra, (PUCMM), Santo Domingo.</h3>
                 </div>
+
+                <div class="col-lg-12 text-left">
+                    <h2 class="section-heading" style="color: white">¡Historia!</h2>
+                    <p class="section-subheading text-muted" style="color: white">El CEICYT nace en el año 2016 por iniciativa del Ministerio de Educación Superior Ciencia y Tecnología (MESCYT), que en conjunto con un grupo de universidades, crean un espacio de reflexión y presentación de proyectos e iniciativas de investigación de estudiantes universitarios. Este primer co- ngreso sembró la semilla que motivó y contribuyó  a fomentar la cultura de la investigación en las IES. </p>
+                    
+                    <p class="section-subheading text-muted" style="color: white">Esa iniciativa continuó en el año 2017 con el II CEICYT, como un evento multidisciplinario que siguió contribuyendo con el desarrollo de la investigación entre los jóvenes estudiantes de las diferentes universidades del país en ciencias básicas y aplicadas. El entusiasmo, el trabajo en equipo y el liderazgo del Viceministerio de Ciencia y Tecnología del MESCYT aseguraron el éxito de los congresos estudiantiles.</p>
+
+                    <p class="section-subheading text-muted" style="color: white">Al arribar a su III versión el CEICYT en este año 2018, aquella semilla sembrada en el 2016 ha crecido y madurado ya constituida en un paradigma de dimensión nacional. Durante estos tres años, bajo la dirección de un Comité Organizador integrado por las universidades UASD, UNAPEC, UNPHU, INTEC, PUCMM, UAPA, UNIBE, UNEV, UFHEC, RIADRU y el MESCYT han contribuido a al desarrollo de la ciencia y la innovación buscando insertar a los jóvenes en la sociedad del conocimiento.</p>
+                </div>
+
+
             </div>
 
             <br>
             
 
-            <div class="row text-center">
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading" style="color: white">Legalización de Documentos</h4>
-                    <p class="text-muted" style="color: white">Legalización de Documentos Académicos de Universidades Activas.</p>
-                    <p class="text-muted" style="color: white">Unidad de atención: Dirección de Control Académico
-                    Horario de servicio: 8:00 a.m. – 4:00 p.m.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading" style="color: white">Documentos Extranjeros</h4>
-                    <p class="text-muted" style="color: white">Legalización de Documentos Académicos de Universidades Activas.</p>
-                    <p class="text-muted" style="color: white">Unidad de atención: Dirección de Control Académico
-                    Horario de servicio: 8:00 a.m. – 4:00 p.m.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading" style="color: white">Solicitud de Exequátur</h4>
-                    <p class="text-muted" style="color: white">Unidad de atención: División de Exequátur, Dirección de Consultoría Jurídica.</p>
-                    <p class="text-muted" style="color: white">Horario de servicio: 8:00 a.m. – 4:00 p.m.</p>
-                </div>
-            </div>
+            
         </div>
 
         <br>
@@ -49,30 +30,7 @@
         <br>
         
 
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading" style="color: white">Pago en linea</h4>
-                    <p class="text-muted" style="color: white">Solicitud y pago en línea del servicio de certificación 
-                        y legalización de documentos académicos.
-                    </p>
-                </div>
-                
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading" style="color: white">Certificados de Ingles de Inmersión</h4>
-                    <p class="text-muted" style="color: white">Impresión de Diplomas Inglés por Inmersión.</p>
-                    
-                </div>
-            </div>
-        </div>
+        
 
 
     </section>
