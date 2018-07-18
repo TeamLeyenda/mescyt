e419a70b4b34900244b6fda216a6d994eeafde82
jmlemoine <jeanmelvinlp27@gmail.com>
2018-07-18 17:30:41 -0400

    Agregando el menu de la frontend

--- frontend/themes/agency/views/layouts/_header.php
+++ frontend/themes/agency/views/layouts/_header.php
@@ -1,4 +1,39 @@
 <div class="container" style="background-color: #235288; border-color: #235288">
+    
+
+
+
+
+
+    
+    <br>
+    <br>
+
+    <nav class="col-lg-12 text-center" style="background-color: #2980B9; border-color: #2980B9">
+        
+        <br>
+        <br>
+        <a type="button" class="btn btn-primary" href="https://www.congresoestudiantil.com/" style="background-color: #235288; border-color: #235288">Inicio</a>
+
+
+
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/congresos-pasados" style="background-color: #235288; color:white; border-color: #235288">Congresos Pasados</a></a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/sobre-ceicyt" style="background-color: #235288; color:white; border-color: #235288">Sobre CEICYT</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/programa" style="background-color: #235288; color:white; border-color: #235288">Programa</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank-1" style="background-color: #235288; color:white; border-color: #235288">Inscripción</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/sometimiento-de-trabajos" style="background-color: #235288; color:white; border-color: #235288">Sometimiento de Trabajo</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank-2" style="background-color: #235288; color:white; border-color: #235288">Comité Organizador</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/patrocinadores" style="background-color: #235288; color:white; border-color: #235288">Patrocinadores</a>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank" style="background-color: #235288; color:white; border-color: #235288">Contactos</a>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        
+        
+      </div>
+      <br>
+    </nav>
+
+
     <div class="column one">
         <div class="top_bar_left clearfix loading" style="width: 920px;">
             <div class="logo">
