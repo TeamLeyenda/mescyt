a417580f9b8f44bbd5ba2e70143ca61b6e5de587
jmlemoine <jeanmelvinlp27@gmail.com>
2018-07-18 19:33:00 -0400

    Seguimos con la frontend

--- frontend/themes/agency/views/layouts/_header.php
+++ frontend/themes/agency/views/layouts/_header.php
@@ -25,6 +25,10 @@
         <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank-2" style="background-color: #235288; color:white; border-color: #235288">Comité Organizador</a>
         <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/patrocinadores" style="background-color: #235288; color:white; border-color: #235288">Patrocinadores</a>
         <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank" style="background-color: #235288; color:white; border-color: #235288">Contactos</a>
+        <a type="button" class="btn btn-secondary" href="login.php" style="background-color: #235288; color:white; border-color: #235288">Log In</a>
+        <br>
+        <br>
+        <a type="button" class="btn btn-secondary" href="https://www.congresoestudiantil.com/blank" style="background-color: #235288; color:white; border-color: #235288">Sign In</a>
 
       <div class="collapse navbar-collapse" id="navbarColor01">
         
