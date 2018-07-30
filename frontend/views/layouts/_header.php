bd5713c0d8983b636d61cc13a51c575f154567d7
jmlemoine <jeanmelvinlp27@gmail.com>
2018-07-30 18:38:30 -0400

    Arreglando el logout

--- frontend/views/layouts/_header.php
+++ frontend/views/layouts/_header.php
@@ -1,10 +1,10 @@
 <?php
 use yii\helpers\Url;
 use yii\helpers\Html;
-<<<<<<< HEAD
 
-=======
->>>>>>> 7bcd7132fe053362b9fb2c2bbb08e92a65a16a66
+
+
+
 ?>
     <nav class="col-lg-12 text-center" style="background-color: #235288; border-color: #235288">
         <a type="button" class="btn btn-secondary" href="<?= Url::to(['/site/index'])?>" style="color:white; font-family: arial">Inicio</a>
@@ -19,10 +19,10 @@ use yii\helpers\Html;
           <a type="button" class="btn btn-secondary" href="<?= Url::to(['/site/login'])?>" style="color:white; font-family: arial">Login</a>
           <a type="button" class="btn btn-secondary" href="<?= Url::to(['/site/signup'])?>" style="color:white; font-family: arial">Sign Up</a>
     <?php   } else { ?>
-          <a type="button" class="btn btn-secondary" >" style="color:white; font-family: arial"><?php
+          <a type="button" class="btn btn-secondary" style="color:white; font-family: arial"><?php
              Html::beginForm(['/site/logout'], 'post');
-             Html::submitButton('Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']);
+             Html::submitButton('Logout (' . Yii::$app->user->identity->username . ')'
+                );
              Html::endForm(); ?>
            </a>
           <a type="button" class="btn btn-secondary" href="#" style="color:white; font-family: arial"><?=Yii::$app->user->identity->username?></a>
