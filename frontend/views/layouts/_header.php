7bcd7132fe053362b9fb2c2bbb08e92a65a16a66
cesar darinel ortiz <cesardarinelortiz@gmail.com>
2018-07-30 18:15:16 -0400

    Agregar cambios en el menu

--- frontend/views/layouts/_header.php
+++ frontend/views/layouts/_header.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Url;
+use yii\helpers\Html;
 ?>
     <nav class="col-lg-12 text-center" style="background-color: #235288; border-color: #235288">
         <a type="button" class="btn btn-secondary" href="<?= Url::to(['/site/index'])?>" style="color:white; font-family: arial">Inicio</a>
@@ -15,11 +16,10 @@ use yii\helpers\Url;
           <a type="button" class="btn btn-secondary" href="<?= Url::to(['/site/signup'])?>" style="color:white; font-family: arial">Sign Up</a>
     <?php   } else { ?>
           <a type="button" class="btn btn-secondary" >" style="color:white; font-family: arial"><?php
-             Html::beginForm(['/site/logout'], 'post')
-             Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout'])
-             Html::endForm() ?>
+             Html::beginForm(['/site/logout'], 'post');
+             Html::submitButton('Logout (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']);
+             Html::endForm(); ?>
            </a>
           <a type="button" class="btn btn-secondary" href="#" style="color:white; font-family: arial"><?=Yii::$app->user->identity->username?></a>
     <?php }?>
